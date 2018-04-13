de94243e87dc9c12297a2900bd75ff455c4813b0
zingu12345 <ashishguptajiit@gmail.com>
2018-04-13 12:51:35 +0530

    Chat Adapter

--- app/src/main/java/volunteer/upay/com/upay/Activities/ChatActivity.java
+++ app/src/main/java/volunteer/upay/com/upay/Activities/ChatActivity.java
@@ -2,6 +2,10 @@ package volunteer.upay.com.upay.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -10,6 +14,9 @@ import volunteer.upay.com.upay.R;
 
 public class ChatActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
+    private RecyclerView mRecyclerViewChat;
+    private EditText mETxtMessage;
+    private ImageView btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,17 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        mRecyclerViewChat = findViewById(R.id.recycler_view_chat);
+        mETxtMessage = findViewById(R.id.edit_text_message);
+        btnSend = findViewById(R.id.btn_send);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage() {
     }
 }
--- app/src/main/java/volunteer/upay/com/upay/Models/ChatUser.java
+++ app/src/main/java/volunteer/upay/com/upay/Models/ChatUser.java
@@ -1,7 +1,15 @@
 package volunteer.upay.com.upay.Models;
 
 public class ChatUser {
-    String email, name, firebaseToken;
+    String email;
+    String name;
+    String firebaseToken;
+    String upayId;
+    String msg;
+
+    public String getMsg() {
+        return msg;
+    }
 
     public String getEmail() {
         return email;
@@ -14,10 +22,18 @@ public class ChatUser {
     public String getFirebaseToken() {
         return firebaseToken;
     }
+    public String getUpayId() {
+        return upayId;
+    }
 
-    public ChatUser(String email, String name, String firebaseToken) {
+
+
+    public ChatUser(String email, String name, String firebaseToken, String upayId, String msg) {
         this.email = email;
         this.name = name;
         this.firebaseToken = firebaseToken;
+        this.upayId = upayId;
+        this.msg = msg;
+
     }
 }
