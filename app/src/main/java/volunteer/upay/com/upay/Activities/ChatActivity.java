a19c7308738b290958dc9107de29a4a0f5fbc636
zingu12345 <ashishguptajiit@gmail.com>
2018-04-13 12:01:58 +0530

    Chat

--- app/src/main/java/volunteer/upay/com/upay/Activities/ChatActivity.java
+++ app/src/main/java/volunteer/upay/com/upay/Activities/ChatActivity.java
@@ -3,13 +3,26 @@ package volunteer.upay.com.upay.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import volunteer.upay.com.upay.R;
 
 public class ChatActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        initDatabase();
+    }
+
+    private void initDatabase() {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        initViews();
+    }
+
+    private void initViews() {
     }
 }
